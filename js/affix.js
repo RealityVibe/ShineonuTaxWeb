54bb3a607bff912970b02e33935d6dedbaa48d89
booleen <2280509982@qq.com>
2018-03-31 10:55:26 +0800

    zhl-03-31上传

---
+++ js/affix.js
@@ -0,0 +1,18 @@
+
+	$(document).ready(function(){
+		$("#myNav").affix({
+		    offset: { 
+		        top: 125 
+		     }
+		  });
+		$("#myNav1").affix({
+		    offset: { 
+		        top: 125 
+		     }
+		  });
+		$("#myNav2").affix({
+		    offset: { 
+		        top: 125 
+		     }
+		  });
+   });
