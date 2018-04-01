5fe269de7ade26edd4fc35441b4bd044af9d3ced
booleen <2280509982@qq.com>
2018-04-01 15:29:34 +0800

    04-01修改yz建议

--- js/affix.js
+++ js/affix.js
@@ -1,18 +1,22 @@
-
+var navHeight = $('.navbar').outerHeight(true) + 30;  
 	$(document).ready(function(){
 		$("#myNav").affix({
 		    offset: { 
-		        top: 125 
+		        top: navHeight
 		     }
 		  });
 		$("#myNav1").affix({
 		    offset: { 
-		        top: 125 
+		        top: navHeight  
 		     }
 		  });
 		$("#myNav2").affix({
 		    offset: { 
-		        top: 125 
+		       top: navHeight  
 		     }
 		  });
+		$('.slide a').click(function(){
+        	var ps = $($(this).attr('href')).offset().top-50;
+        	$('html,body').delay(100).animate({scrollTop:ps}, 300)
+    	});
    });
